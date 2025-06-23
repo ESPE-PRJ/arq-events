494876eecdd3a29d947f2a7a7b7a79b91f083d6f
Ooops <dev.seb.3745@gmail.com>
2025-06-23 10:20:27 -0500

    orm added

--- ordenes/src/app.module.ts
+++ ordenes/src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import serverConfig from 'config/server.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from 'config/database.config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Module({
   imports: [
     HealthModule,
+    TypeOrmModule.forRootAsync(
+      databaseConfig.asProvider() as Partial<PostgresConnectionOptions>,
+    ),
     ConfigModule.forRoot({
       envFilePath: '.env',
       load: [serverConfig],
